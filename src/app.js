273e34a9482f2ab163f2ed8bfa437f3134dc2f73
anishazahan <anishazahan13@gmail.com>
2023-05-21 16:15:05 +0600

    simple error handling with the use of http error

---
+++ src/app.js
@@ -0,0 +1,64 @@
+
+const express = require('express');
+const app = express();
+const morgan = require("morgan");
+const createError = require('http-errors')
+
+
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const isLoggedIn =(req,res,next)=>{
+
+    const user = true;
+    if(user){
+        req.body.id= 100;
+        next()
+    }else{
+       return res.status(401).json({
+            message:"unauthorized user"
+        })
+    }
+ 
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!xcvxb')
+})
+
+app.get('/api/user',isLoggedIn, (req, res) => {
+    console.log(req.body.id);
+  res.status(200).send({
+    message:"user progile is login"
+  })
+})
+
+
+// client error handeling--
+
+app.use((req,res,next)=>{
+//   res.status(404).json({
+//     message:"route not found"
+//   })
+
+
+// createError()
+//   next()
+
+next(createError(404,"route not found"))
+})
+
+// server error  = all the errors
+
+app.use((err, req, res, next) => {
+    
+    return res.status(err.status || 500).json({
+        success:false,
+        message:err.message,
+    })
+  })
+
+
+  module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,39 +1,8 @@
 
-const express = require('express');
-const app = express();
-const morgan = require("morgan");
+
+const app = require('./app');
+
 const port = 5000
-
-
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-const isLoggedIn =(req,res,next)=>{
-
-    const user = true;
-    if(user){
-        req.body.id= 100;
-        next()
-    }else{
-       return res.status(401).json({
-            message:"unauthorized user"
-        })
-    }
- 
-}
-
-app.get('/', (req, res) => {
-  res.send('Hello World!xcvxb')
-})
-
-app.get('/api/user',isLoggedIn, (req, res) => {
-    console.log(req.body.id);
-  res.status(200).send({
-    message:"user progile is login"
-  })
-})
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
