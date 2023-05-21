a124a1098cd706e97243763c26ace7457164d32e
anishazahan <anishazahan13@gmail.com>
2023-05-21 12:07:11 +0600

    api testing with the use off thunder client and postman

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const morgan = require("morgan");
 const port = 5000
+
+
+app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
   res.send('Hello World!xcvxb')
+})
+app.post('/', (req, res) => {
+  res.send('post method')
+})
+app.put('/', (req, res) => {
+  res.send('put method')
+})
+app.delete('/', (req, res) => {
+  res.send('delete method')
 })
 
 app.listen(port, () => {
