74116c3b4c74f09a72f5d7782ed550bb4b861306
anishazahan <anishazahan13@gmail.com>
2023-05-26 14:58:50 +0600

    use mongoose pakage

--- src/secret.js
+++ src/secret.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const serverPort = process.env.SERVER_PORT || 5001;
+const mongoAtlasURL = process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/eCommerceMern";
 
 
 module.exports={
     serverPort,
+    mongoAtlasURL
+
 }
--- src/server.js
+++ src/server.js
@@ -6,3 +6,7 @@ const { serverPort } = require('./secret');
 app.listen(serverPort, () => {
   console.log(`Example app listening on port ${serverPort}`)
 })
+
+// mern-ecommerce
+
+// Am0DIFNUotfk9XXz
