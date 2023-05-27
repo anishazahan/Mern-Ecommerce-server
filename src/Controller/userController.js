73e9122cdfcca6f07fae4dac421c199284c4faeb
anishazahan <anishazahan13@gmail.com>
2023-05-27 23:11:37 +0600

    custom succes message

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const userModel = require('../Model/user.model');
+const { successResponse } = require('./responseController');
 
 const getUsers = async(req, res,next) => {
    try {
@@ -26,15 +27,30 @@ const getUsers = async(req, res,next) => {
 
       if(!users) throw createError(404,"No Users Found")
 
-    res.status(200).send({
-      message:"users get successfully",
-      users,
-      pagination:{
-        totalPages : Math.ceil(count / limit),
-        currentPage:page,
-        previousPage: page - 1 > 0 ? page - 1 : null,
-        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+    // res.status(200).send({
+    //   message:"users get successfully",
+    //   users,
+    //   pagination:{
+    //     totalPages : Math.ceil(count / limit),
+    //     currentPage:page,
+    //     previousPage: page - 1 > 0 ? page - 1 : null,
+    //     nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
 
+    //   }
+    // })
+
+    return successResponse(res,{
+      statusCode:200,
+      message:"Users get successfully",
+      payload:{
+        users,
+        pagination:{
+          totalPages : Math.ceil(count / limit),
+          currentPage:page,
+          previousPage: page - 1 > 0 ? page - 1 : null,
+          nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+  
+        }
       }
     })
     
